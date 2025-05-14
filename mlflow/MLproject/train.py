246e7f79368d6eb346bd06f990450334d2d53da9
echerednikov <echerednikov@diasoft.ru>
2025-05-14 18:08:26 +0300

    lab2 смена настройки

--- mlflow/MLproject/train.py
+++ mlflow/MLproject/train.py
@@ -1,59 +1,23 @@
+import argparse
 import mlflow
-import mlflow.pytorch
+import mlflow.sklearn
+from sklearn.linear_model import Ridge
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
+parser = argparse.ArgumentParser()
+parser.add_argument("--alpha", type=float, required=True)
+args = parser.parse_args()
 
-from preprocess import BratsDataset
+X, y = load_diabetes(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-configure_mlflow()
+model = Ridge(alpha=args.alpha)
+model.fit(X_train, y_train)
+preds = model.predict(X_test)
+rmse = mean_squared_error(y_test, preds, squared=False)
 
-params = {
-    "epochs": 10,
-    "batch_size": 2,
-    "lr": 1e-4,
-    "model": "UNet",
-    "optimizer": "Adam"
-}
-
-with mlflow.start_run():
-    mlflow.log_params(params)
-
-    # Датасет
-    train_dataset = BratsDataset(split='train')
-    val_dataset = BratsDataset(split='val')
-    train_loader = DataLoader(train_dataset, batch_size=params["batch_size"], shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=1)
-
-    model = UNet()
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters(), lr=params["lr"])
-
-    for epoch in range(params["epochs"]):
-        model.train()
-        train_loss = 0.0
-        for images, masks in train_loader:
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, masks)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-
-        avg_train_loss = train_loss / len(train_loader)
-        mlflow.log_metric("train_loss", avg_train_loss, step=epoch)
-
-        model.eval()
-        val_loss = 0.0
-        with torch.no_grad():
-            for images, masks in val_loader:
-                outputs = model(images)
-                loss = criterion(outputs, masks)
-                val_loss += loss.item()
-
-        avg_val_loss = val_loss / len(val_loader)
-        mlflow.log_metric("val_loss", avg_val_loss, step=epoch)
-
-    mlflow.pytorch.log_model(model, "model")
+mlflow.log_param("alpha", args.alpha)
+mlflow.log_metric("rmse", rmse)
+mlflow.sklearn.log_model(model, "model")

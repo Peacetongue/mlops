3d362d9660b04e6831e5a68f0f255cf3c43ea76b
echerednikov <69628954+Sergiys-web@users.noreply.github.com>
2025-05-28 20:29:34 +0300

    lab3 init

--- bentoML/save_model.py
+++ bentoML/save_model.py
@@ -1,6 +1,25 @@
-from transformers import pipeline
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.utils import to_categorical
 import bentoml
 
-summarizer = pipeline("summarization", model="cointegrated/rut5-base-absum")
 
-bentoml.transformers.save_model("text_summarizer", summarizer)
+(x_train, y_train), _ = cifar10.load_data()
+x_train = x_train.astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+    MaxPooling2D((2,2)),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5, batch_size=64, verbose=2)
+
+
+bentoml.keras.save_model("cifar10_cnn", model)
--- bentoML/service.py
+++ bentoML/service.py
@@ -1,13 +1,30 @@
 import bentoml
-from bentoml.io import JSON
+from bentoml.io import Image, JSON
+import numpy as np
+from PIL import Image as PILImage
 
-model_ref = bentoml.transformers.get("text_summarizer:latest")
-summarizer_runner = model_ref.to_runner()
+model_ref = bentoml.keras.get("cifar10_cnn:latest")
+model_runner = model_ref.to_runner()
 
-svc = bentoml.Service("summarizer_service", runners=[summarizer_runner])
+svc = bentoml.Service("cifar10_classifier", runners=[model_runner])
 
-@svc.api(input=JSON(), output=JSON())
-async def summarize(input_json: dict):
-    text = input_json["text"]
-    result = await summarizer_runner.async_run(text)
-    return {"summary": result[0]["summary_text"]}
+class_names = [
+    "airplane", "automobile", "bird", "cat", "deer",
+    "dog", "frog", "horse", "ship", "truck"
+]
+
+@svc.api(input=Image(), output=JSON())
+async def classify(input_image: PILImage.Image):
+    image = input_image.resize((32, 32)).convert("RGB")
+    image_array = np.array(image).astype("float32") / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
+
+    prediction = await model_runner.async_run(image_array)
+    predicted_class = int(np.argmax(prediction))
+    class_name = class_names[predicted_class]
+
+    return {
+        "predicted_class_id": predicted_class,
+        "class_name": class_name,
+        "confidence": float(np.max(prediction))
+    }
